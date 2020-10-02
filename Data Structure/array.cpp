404b014f62477b0a507ae26d1305ab7e162fd646
blackcrabb <57863214+blackcrabb@users.noreply.github.com>
2020-10-02 15:24:49 +0530

    added array(basic)

---
+++ Data Structure/array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+// declare an array, can be of any datatype
+int A[50];
+
+//initialising an array
+for(int i=0;i<6;i++)
+{
+	cin>>A[i];
+}
+
+//displaying array
+for(int i=0;i<6;i++)
+{
+	cout<<A[i];
+}
+}
